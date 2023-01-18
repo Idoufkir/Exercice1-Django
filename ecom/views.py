76eb7d8a6968f01722a69bd2ea3846b34acd8b2b
Mustafa-Dev-2 <DEVELOPER-2@PROFONDE.LOCAL>
2023-01-18 15:58:09 +0100

    Post Request

--- ecom/views.py
+++ ecom/views.py
@@ -8,8 +8,30 @@ from .serializers import ProductSerializer
 
 
 class LatestProductList(APIView):
-    def get(self, request):
-        products = Product.objects.all()[0:4]
+    def get_queryset(self):
+        products = Product.objects.all()
+        return products
+
+    def get(self, request, *args, **kwargs):
+        products = self.get_queryset()
         serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        product_data = request.data
+
+        new_product = Product.objects.create(
+            reference=product_data["reference"],
+            name=product_data["name"],
+            image=product_data["image"],
+            description=product_data["description"]
+        )
+
+        new_product.save()
+
+        serializer = ProductSerializer(new_product)
+        serializer.is_valid()
+
         return Response(serializer.data)
 
